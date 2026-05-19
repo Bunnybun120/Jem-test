f0b6217b93b5c10704655bc5d7b600f6d5e28c21
Bunnybun120 <yahviryans@gmail.com>
2026-05-19 16:45:43 -0400

    Create Music.js

---
+++ Music.js
@@ -0,0 +1,10 @@
+const audio =
+document.getElementById("audio");
+
+document
+.getElementById("playBtn")
+.addEventListener("click", () => {audio.play();});
+
+document
+.getElementById("pauseBtn")
+.addEventListener("click", () => {audio.pause();});
