7ab734e7500b0dd0f2b4a51edca144fb0453d5d2
Bunnybun120 <yahviryans@gmail.com>
2026-05-20 16:46:39 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,25 +40,57 @@ drag=false;
 
 /* CHAT */
 
+document.addEventListener("DOMContentLoaded",()=>{
+
 const chatBox=document.getElementById("chatBox");
 
 if(!chatBox) return;
 
-const chatInput=document.getElementById("chatInput");
-const chatSend=document.getElementById("chatSend");
-const usernameInput=document.getElementById("usernameInput");
-const colorInput=document.getElementById("colorInput");
+/* INPUTS */
+
+const usernameInput=
+document.getElementById("usernameInput");
+
+const pfpInput=
+document.getElementById("pfpInput");
+
+const bioInput=
+document.getElementById("bioInput");
+
+const colorInput=
+document.getElementById("colorInput");
+
+const chatInput=
+document.getElementById("chatInput");
+
+const chatSend=
+document.getElementById("chatSend");
 
 /* FIREBASE */
 
 const firebaseConfig = {
-apiKey: "AIzaSyAQkx8r6WwtLAjFfFSmlGEOTcCFvWb7hWI",
-authDomain: "chatroom-39c7a.firebaseapp.com",
-databaseURL: "https://chatroom-39c7a-default-rtdb.firebaseio.com",
-projectId: "chatroom-39c7a",
-storageBucket: "chatroom-39c7a.firebasestorage.app",
-messagingSenderId: "590743257861",
-appId: "1:590743257861:web:e386928c084ba704ca2d6c"
+
+apiKey:
+"AIzaSyAQkx8r6WwtLAjFfFSmlGEOTcCFvWb7hWI",
+
+authDomain:
+"chatroom-39c7a.firebaseapp.com",
+
+databaseURL:
+"https://chatroom-39c7a-default-rtdb.firebaseio.com",
+
+projectId:
+"chatroom-39c7a",
+
+storageBucket:
+"chatroom-39c7a.firebasestorage.app",
+
+messagingSenderId:
+"590743257861",
+
+appId:
+"1:590743257861:web:e386928c084ba704ca2d6c"
+
 };
 
 if(!firebase.apps.length){
@@ -80,10 +112,16 @@ db.ref("messages").push({
 username:
 usernameInput.value || "anon",
 
-text:text,
+pfp:
+pfpInput.value || "",
+
+bio:
+bioInput.value || "",
 
 color:
 colorInput.value || "#ff4fd8",
+
+text:text,
 
 time:Date.now()
 
@@ -93,9 +131,14 @@ chatInput.value="";
 
 }
 
-chatSend.addEventListener("click",sendMessage);
+chatSend.addEventListener(
+"click",
+sendMessage
+);
 
-chatInput.addEventListener("keydown",e=>{
+chatInput.addEventListener(
+"keydown",
+e=>{
 
 if(e.key==="Enter"){
 sendMessage();
@@ -111,23 +154,53 @@ db.ref("messages")
 
 const data=snap.val();
 
-const div=document.createElement("div");
+const div=
+document.createElement("div");
 
 div.className="chat-message";
 
 div.innerHTML=`
 
-<span style="color:${data.color}; font-weight:bold;">
-${data.username}
-</span>
+<img
+class="chat-pfp"
+src="${data.pfp || 'https://i.imgur.com/8Km9tLL.png'}">
 
-: ${data.text}
+<div class="chat-bubble">
+
+<div
+style="
+color:${data.color};
+font-weight:bold;
+font-size:16px;
+">
+
+${data.username}
+
+</div>
+
+<div
+style="
+opacity:0.7;
+font-size:12px;
+margin-bottom:6px;
+">
+
+${data.bio || ""}
+
+</div>
+
+<div>
+${data.text}
+</div>
+
+</div>
 
 `;
 
 chatBox.appendChild(div);
 
-chatBox.scrollTop=chatBox.scrollHeight;
+chatBox.scrollTop=
+chatBox.scrollHeight;
 
 });
 
