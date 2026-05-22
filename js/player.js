4ffe9ba26cc843d80600a59e37d95de398629804
Bunnybun120 <yahviryans@gmail.com>
2026-05-22 14:10:23 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,49 @@
+const playBtn =
+document.getElementById(
+"playBtn"
+);
 
+const pauseBtn =
+document.getElementById(
+"pauseBtn"
+);
+
+playBtn.onclick = ()=>{
+
+const url =
+document.getElementById(
+"youtubeInput"
+).value;
+
+if(!url){
+
+alert(
+"paste a youtube url"
+);
+
+return;
+
+}
+
+document.getElementById(
+"trackTitle"
+).innerText =
+
+"NOW PLAYING";
+
+window.open(
+url,
+"_blank"
+);
+
+};
+
+pauseBtn.onclick = ()=>{
+
+document.getElementById(
+"trackTitle"
+).innerText =
+
+"STOPPED";
+
+};
