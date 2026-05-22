9cd477dbb9e2a4000c6d913ff6371383e1f2c86a
Bunnybun120 <yahviryans@gmail.com>
2026-05-22 19:35:00 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -8,6 +8,11 @@ document.getElementById(
 "pauseBtn"
 );
 
+const player =
+document.getElementById(
+"youtubePlayer"
+);
+
 playBtn.onclick = ()=>{
 
 const url =
@@ -25,20 +30,53 @@ return;
 
 }
 
+let videoId = "";
+
+/* NORMAL YOUTUBE */
+
+if(url.includes("watch?v=")){
+
+videoId =
+url.split("watch?v=")[1]
+.split("&")[0];
+
+}
+
+/* SHORT LINKS */
+
+if(url.includes("youtu.be/")){
+
+videoId =
+url.split("youtu.be/")[1]
+.split("?")[0];
+
+}
+
+if(videoId === ""){
+
+alert(
+"invalid youtube link"
+);
+
+return;
+
+}
+
+player.src =
+
+`https://www.youtube.com/embed/${videoId}?autoplay=1`;
+
 document.getElementById(
 "trackTitle"
 ).innerText =
 
 "NOW PLAYING";
 
-window.open(
-url,
-"_blank"
-);
-
 };
 
 pauseBtn.onclick = ()=>{
+
+player.src = "";
 
 document.getElementById(
 "trackTitle"
