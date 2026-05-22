756749b87f13164c3d53bac3635b21cb0425ff66
Bunnybun120 <yahviryans@gmail.com>
2026-05-22 14:12:56 -0400

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -1,1 +1,35 @@
+function setTheme(theme){
 
+document.getElementById(
+"themeStylesheet"
+).href =
+
+`themes/${theme}.css`;
+
+localStorage.setItem(
+"theme",
+theme
+);
+
+}
+
+window.addEventListener(
+"load",
+()=>{
+
+const savedTheme =
+localStorage.getItem(
+"theme"
+);
+
+if(savedTheme){
+
+document.getElementById(
+"themeStylesheet"
+).href =
+
+`themes/${savedTheme}.css`;
+
+}
+
+});
