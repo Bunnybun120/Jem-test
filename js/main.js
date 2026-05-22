86f27f4c70f30d32e7ea8a3c4e165e1530087228
Bunnybun120 <yahviryans@gmail.com>
2026-05-21 21:45:19 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,7 +65,50 @@ GET VIDEO ID
 ========================================= */
 
 function getYoutubeVideoID(url){
+/* =========================================
+NOW PLAYING
+========================================= */
 
+async function updateNowPlaying(videoID){
+
+if(!nowPlaying) return;
+
+try{
+
+const response =
+await fetch(
+
+`https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoID}`
+
+);
+
+const data =
+await response.json();
+
+if(data.title){
+
+nowPlaying.innerText =
+
+"NOW PLAYING :: " +
+data.title;
+
+}else{
+
+nowPlaying.innerText =
+
+"NOW PLAYING :: unknown track";
+
+}
+
+}catch{
+
+nowPlaying.innerText =
+
+"NOW PLAYING :: unable to load";
+
+}
+
+}
 try{
 
 const parsed =
@@ -252,49 +295,7 @@ updateNowPlaying(
 videoID
 );
 if(!videoID){
-/* =========================================
-NOW PLAYING
-========================================= */
-
-async function updateNowPlaying(videoID){
-
-if(!nowPlaying) return;
-
-try{
-
-const response =
-await fetch(
-
-`https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoID}`
-
-);
-
-const data =
-await response.json();
-
-if(data.title){
-
-nowPlaying.innerText =
-
-"NOW PLAYING :: " +
-data.title;
-
-}else{
-
-nowPlaying.innerText =
-
-"NOW PLAYING :: unknown track";
-
-}
-
-}catch{
-
-nowPlaying.innerText =
-
-"NOW PLAYING :: unable to load";
-
-}
-
+/
 }
 alert(
 "Invalid YouTube link."
