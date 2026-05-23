54f8c0b0946d1e4ccf4048af744fe1df1e98108b
Bunnybun120 <yahviryans@gmail.com>
2026-05-22 20:06:14 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-/* CLOCK */
+/* =========================================
+CLOCK
+========================================= */
 
 function updateClock(){
 
 const now =
 new Date();
 
+const time =
+
+now.toLocaleTimeString(
+[],
+{
+hour:'2-digit',
+minute:'2-digit'
+}
+);
+
+const clock =
 document.getElementById(
 "clock"
-).innerText =
+);
 
-now.toLocaleTimeString();
+if(clock){
+
+clock.innerText = time;
+
+}
 
 }
 
@@ -19,3 +36,97 @@ updateClock,
 );
 
 updateClock();
+
+/* =========================================
+WINDOWS
+========================================= */
+
+function openWindow(id){
+
+const win =
+document.getElementById(id);
+
+if(win){
+
+win.style.display = "flex";
+
+}
+
+}
+
+function closeWindow(id){
+
+const win =
+document.getElementById(id);
+
+if(win){
+
+win.style.display = "none";
+
+}
+
+}
+
+/* =========================================
+CHATROOM
+========================================= */
+
+const sendButton =
+document.getElementById(
+"sendMessage"
+);
+
+const clearButton =
+document.getElementById(
+"clearChat"
+);
+
+if(sendButton){
+
+sendButton.onclick = ()=>{
+
+const input =
+document.getElementById(
+"chatInput"
+);
+
+const messages =
+document.getElementById(
+"chatMessages"
+);
+
+if(input.value.trim() === "")
+return;
+
+const msg =
+document.createElement(
+"div"
+);
+
+msg.style.marginBottom = "12px";
+
+msg.innerText =
+input.value;
+
+messages.appendChild(msg);
+
+input.value = "";
+
+messages.scrollTop =
+messages.scrollHeight;
+
+};
+
+}
+
+if(clearButton){
+
+clearButton.onclick = ()=>{
+
+document.getElementById(
+"chatMessages"
+).innerHTML = "";
+
+};
+
+}
