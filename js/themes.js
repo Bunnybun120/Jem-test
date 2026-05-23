b067bb73abd50b36cfe77caabfc12f552dedc54c
Bunnybun120 <yahviryans@gmail.com>
2026-05-23 17:55:24 -0400

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -1,35 +1,72 @@
 function setTheme(theme){
 
+const bg =
+document.getElementById(
+"themeBackground"
+);
+
+bg.innerHTML = "";
+
 document.getElementById(
 "themeStylesheet"
 ).href =
-
 `themes/${theme}.css`;
 
+if(theme === "vaporwave"){
+
+bg.innerHTML = `
+
+<div class="vapor-stars"></div>
+<div class="vapor-sun"></div>
+<div class="vapor-city"></div>
+<div class="vapor-grid"></div>
+<div class="vapor-fog"></div>
+<div class="vapor-vhs"></div>
+
+`;
+
+}
+
+if(theme === "aero"){
+
+bg.innerHTML = `
+
+<div class="aero-clouds"></div>
+<div class="aero-water"></div>
+<div class="aero-bubbles"></div>
+<div class="aero-light"></div>
+
+`;
+
+}
+
+if(theme === "rosalina"){
+
+bg.innerHTML = `
+
+<div class="rosa-nebula"></div>
+<div class="rosa-stars"></div>
+<div class="rosa-sparkles"></div>
+<div class="rosa-fog"></div>
+
+`;
+
+}
+
 localStorage.setItem(
-"theme",
+"selectedTheme",
 theme
 );
 
 }
 
-window.addEventListener(
-"load",
-()=>{
+/* =========================================
+LOAD SAVED THEME
+========================================= */
 
 const savedTheme =
 localStorage.getItem(
-"theme"
-);
+"selectedTheme"
+) || "vaporwave";
 
-if(savedTheme){
-
-document.getElementById(
-"themeStylesheet"
-).href =
-
-`themes/${savedTheme}.css`;
-
-}
-
-});
+setTheme(savedTheme);
